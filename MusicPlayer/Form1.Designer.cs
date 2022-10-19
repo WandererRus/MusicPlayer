49efebfbf76ca23136c78856f8ebb49e467b6d27
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-10-19 21:39:31 +0300

    Добавьте файлы проекта.

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(169, 93);
+            this.btn_Open.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(214, 77);
+            this.btn_Open.TabIndex = 0;
+            this.btn_Open.Text = "Открыть";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Location = new System.Drawing.Point(400, 93);
+            this.btn_Play.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(214, 77);
+            this.btn_Play.TabIndex = 1;
+            this.btn_Play.Text = "Играть";
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(631, 93);
+            this.btn_Stop.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(214, 77);
+            this.btn_Stop.TabIndex = 2;
+            this.btn_Stop.Text = "Стоп";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 50F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 263);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Play);
+            this.Controls.Add(this.btn_Open);
+            this.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn_Open;
+        private Button btn_Play;
+        private Button btn_Stop;
+        private OpenFileDialog ofd;
+    }
+}
